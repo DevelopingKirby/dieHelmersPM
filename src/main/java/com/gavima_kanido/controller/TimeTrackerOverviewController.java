1a0c9b50292f79cd8e4c0660da29f9e4f6bec9af
Katherine <katherine.kuhle@googlemail.com>
2021-07-21 20:50:29 +0200

    added file PersonalData.fxml

---
+++ src/main/java/com/gavima_kanido/controller/TimeTrackerOverviewController.java
@@ -0,0 +1,11 @@
+package com.gavima_kanido.controller;
+
+import com.gavima_kanido.models.User;
+
+public class TimeTrackerOverviewController {
+    private User user;
+
+    public TimeTrackerOverviewController(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/gavima_kanido/controller/TimeTrackerProjectsController.java
@@ -0,0 +1,12 @@
+
+package com.gavima_kanido.controller;
+
+import com.gavima_kanido.models.User;
+
+public class TimeTrackerProjectsController {
+    private User user;
+
+    public TimeTrackerProjectsController(User user) {
+        this.user = user;
+    }
+}
