7499a3ecfe03ae63541cf7c0cf22558f28e7b6a3
maxiking21 <max.fromm96@gmail.com>
2021-07-21 21:24:38 +0200

    added missing Controllers

---
+++ src/main/java/com/gavima_kanido/controller/TimeTrackerOverviewController.java
@@ -0,0 +1,13 @@
+package com.gavima_kanido.controller;
+
+import com.gavima_kanido.models.User;
+
+public class TimeTrackerOverviewController {
+
+    private User user;
+
+    public TimeTrackerOverviewController(User stageUser){
+        this.user = user;
+    }
+
+}
---
+++ src/main/java/com/gavima_kanido/controller/TimeTrackerProjectsController.java
@@ -0,0 +1,13 @@
+package com.gavima_kanido.controller;
+
+import com.gavima_kanido.models.User;
+
+public class TimeTrackerProjectsController {
+
+    private User user;
+
+    public TimeTrackerProjectsController(User stageUser){
+        this.user = user;
+    }
+
+}
