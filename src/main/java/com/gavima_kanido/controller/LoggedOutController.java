2f89c78fdbc3f11283061b86e4650facff910354
DevelopingKirby <58995819+DevelopingKirby@users.noreply.github.com>
2021-07-24 17:13:10 +0200

    Update LoggedOutController.java

--- src/main/java/com/gavima_kanido/controller/LoggedOutController.java
+++ src/main/java/com/gavima_kanido/controller/LoggedOutController.java
@@ -3,8 +3,11 @@ package com.gavima_kanido.controller;
 import com.gavima_kanido.models.User;
 
 public class LoggedOutController {
+    
+    private User user
 
     public LoggedOutController(User stageUser) {
+        this.user = stageUser
     }
 
 }
