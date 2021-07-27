81b274cbcfc636387ada322e60c8839b20ff6e32
maxiking21 <max.fromm96@gmail.com>
2021-07-27 22:54:03 +0200

    added User Properties, implemented PersonalData

--- src/main/java/com/gavima_kanido/controller/PersonalDataController.java
+++ src/main/java/com/gavima_kanido/controller/PersonalDataController.java
@@ -13,6 +13,7 @@ import com.gavima_kanido.models.User;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -34,6 +35,40 @@ public class PersonalDataController {
     private Button btnTeams;
 
     @FXML
+    private Label lblUserRef;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private Label lblDepartment;
+
+    @FXML
+    private Label lblTeam;
+
+    @FXML
+    private Label lblEMail;
+
+    @FXML
+    private Label lblPhone;
+
+    @FXML
+    private Label lblZip;
+
+    @FXML
+    private Label lblCountry;
+
+    @FXML
+    private Label lblCity;
+
+    @FXML
+    private Label lblStreet;
+
+    @FXML
+    private Label lblSuperior;
+
+
+    @FXML
     void handleButtonAction(MouseEvent event) throws IOException {
 
         if (event.getSource() == topmenu_dashboard) {
@@ -45,6 +80,24 @@ public class PersonalDataController {
         else if (event.getSource() == btnTeams) {
             StageHandler.changeToTeams((Stage) btnTeams.getScene().getWindow(), getClass());
         }
+    }
+
+    @FXML
+    public void initialize(){
+        
+        lblUserRef.setText(user.getUserRef());
+        lblName.setText(user.getFirstName().substring(0, 1).toUpperCase() + user.getFirstName().substring(1) + " " + user.getName().substring(0, 1).toUpperCase() + user.getName().substring(1));
+        lblDepartment.setText(user.getDepartment());
+        lblTeam.setText(user.getTeam());
+        lblEMail.setText(user.getEMail());
+        lblPhone.setText(user.getPhoneNumber());
+        lblCity.setText(user.getCity());
+        lblCountry.setText(user.getCountry());
+        lblZip.setText(Integer.toString(user.getZip()));
+        lblStreet.setText(user.getStreet());
+        lblSuperior.setText(user.getSuperiorName());
+
+
     }
 
 } 
